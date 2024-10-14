5ef39f78c1b1d0809f81720d28f9ddacba630923
Mafuyu <camiloandrescadena1024@gmail.com>
2024-10-14 18:40:41 -0500

    Descripcion de los cambios

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-// Middleware para poder leer datos de la petición
+// Middleware para analizar parámetros de consulta
 app.use(express.json());
 
 app.get('/gps', (req, res) => {
     const latitude = req.query.latitude;
     const longitude = req.query.longitude;
-
+    
     if (latitude && longitude) {
-        console.log(`Recibidos: Latitud ${latitude}, Longitud ${longitude}`);
-        res.status(200).send(`Datos recibidos: Latitud ${latitude}, Longitud ${longitude}`);
+        // Procesa los datos y envía una respuesta
+        res.status(200).send(`Latitud: ${latitude}, Longitud: ${longitude}`);
     } else {
-        res.status(400).send('Datos de GPS no proporcionados');
+        res.status(400).send('Faltan parámetros');
     }
 });
-
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
 });
-
